575c0ab204518183d19c52cc804fd565e1e22fa2
I.A.M.S <islammuhid962@gmail.com>
2025-09-26 11:32:36 +0600

    Update antibd.js

--- Script/events/antibd.js
+++ Script/events/antibd.js
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - পাগল ছাগল তুই নিকনেম চেঞ্জ করতে পারবি না 😹\n শুধু আমার বস উল্লাস চেঞ্জ করতে পারবে🖐`}, threadID);
+       return api.sendMessage({ body: `${info.name} - আডমিন ছাড়া নিকনেম পাল্টানো যাবে না বসস /n যদি চাস পাল্টাতে ❦ 𝚂Ꭵ𝙶ɱꪖ ☣ 𝕭𝖔𝒚모 রে ডাক দেই🖐`}, threadID);
     }  
         }
